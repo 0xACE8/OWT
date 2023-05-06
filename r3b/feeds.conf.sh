5d5096d01324091d96ff86d7ca9291ae8b964c61
0xACE8 <19428059+0xACE8@users.noreply.github.com>
2023-05-06 06:37:04 -0700

    Update and rename feeds.conf.sh to f33ds.sh

--- r3b/feeds.conf.sh
+++
@@ -1,5 +0,0 @@
-# Add a feed source
-echo 'src-git ace https://github.com/0xACE8/ace-packages' >>feeds.conf.default
-echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
-
---
+++ threeb/f33ds.sh
@@ -0,0 +1,5 @@
+# Add a feed source
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git ace https://github.com/0xACE8/ace-packages' >>feeds.conf.default
+echo 'src-git roacn https://github.com/roacn/openwrt-packages' >>feeds.conf.default
