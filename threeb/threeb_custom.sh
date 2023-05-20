3001c015b994f60416dc0493a4be19f293d22a7f
0xACE8 <19428059+0xACE8@users.noreply.github.com>
2023-05-20 08:24:41 -0700

    Update threeb_custom.sh

--- threeb/threeb_custom.sh
+++ threeb/threeb_custom.sh
@@ -8,7 +8,7 @@
 sed -i 's/192.168.1.1/192.168.177.99/g' package/base-files/files/bin/config_generate
 
 #2. Modify Hostname
-sed -i '/uci commit system/i\uci set system.@system[0].hostname='OpenwrtBP'' package/lean/default-settings/files/zzz-default-settings
+sed -i '/uci commit system/i\uci set system.@system[0].hostname='Openwrt_Bypass'' package/lean/default-settings/files/zzz-default-settings
 
 #3. Password is ********
 sed -i 's/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/root:$1$4xKZB45Q$w0CPT5M6vBWbYNmSWuxfU.:19007:0:99999:7:::/g' package/lean/default-settings/files/zzz-default-settings
@@ -17,9 +17,7 @@ sed -i 's/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/root:$1$4xKZB45
 sed -i "s/OpenWrt /0xACE7 build $(TZ=UTC-3 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
 
 #5. Change luci list name
-#sed -i 's/"Argon 主题设置"/"主题设置"/g' feeds/kenzo/luci-app-argon-config/po/zh-cn/argon-config.po
-sed -i 's/"Argonne 主题设置"/"主题设置"/g' feeds/kenzo/luci-app-argonne-config/po/zh-cn/argonne-config.po
-mv feeds/0xACE8/luci-theme-argonne/htdocs/luci-static/argonne/img/argon.svg feeds/kenzo/luci-theme-argonne/htdocs/luci-static/argonne/img/argonne.svg
+sed -i 's/"Argonne 主题设置"/"主题设置"/g' feeds/kenzo/luci-app-argone-config/po/zh-cn/argone-config.po
 sed -i 's/"Turbo ACC 网络加速"/"网络加速"/g' feeds/luci/applications/luci-app-turboacc/po/zh-cn/turboacc.po
 
 #6. Change dns server
@@ -36,6 +34,27 @@ sed -i "11i uci set network.lan6.reqaddress='try'" package/lean/default-settings
 sed -i "12i uci set network.lan6.reqprefix='auto'" package/lean/default-settings/files/zzz-default-settings
 sed -i "13i uci set network.lan6.peerdns='0'" package/lean/default-settings/files/zzz-default-settings
 sed -i "14i uci set network.lan6.dns='::1'" package/lean/default-settings/files/zzz-default-settings
+#sed -i '/revert_dns/d' feeds/luci/applications/luci-app-turboacc/root/etc/init.d/turboacc
+
+#6. Boost UDP
+echo '# optimize udp' >>package/base-files/files/etc/sysctl.d/10-default.conf
+echo 'net.core.rmem_max=26214400' >>package/base-files/files/etc/sysctl.d/10-default.conf
+echo 'net.core.rmem_default=26214400' >>package/base-files/files/etc/sysctl.d/10-default.conf
+echo 'net.core.wmem_max=26214400' >>package/base-files/files/etc/sysctl.d/10-default.conf
+echo 'net.core.wmem_default=26214400' >>package/base-files/files/etc/sysctl.d/10-default.conf
+echo 'net.core.netdev_max_backlog=2048' >>package/base-files/files/etc/sysctl.d/10-default.conf
+
+#7. Update kcptun
+rm -rf feeds/packages/net/kcptun
+git clone https://github.com/0xACE8/kcptun.git feeds/packages/net/kcptun
+
+#8. Update udpspeeder
+rm -rf feeds/packages/net/udpspeeder
+git clone https://github.com/0xACE8/udpspeeder.git feeds/packages/net/udpspeeder
+
+#9. Update dnscrypt-proxy2
+rm -rf feeds/packages/net/dnscrypt-proxy2
+git clone https://github.com/0xACE8/dnscrypt-proxy2.git feeds/packages/net/dnscrypt-proxy2
 
 #10. Change to my banner
 sudo rm package/base-files/files/etc/banner
