c91a1fa1f7f01339ef7ec9a2f9505419e5da88a6
0xACE8 <19428059+0xACE8@users.noreply.github.com>
2023-08-11 04:55:16 -0700

    Update f33ds.sh

--- mt7620a/f33ds.sh
+++ mt7620a/f33ds.sh
@@ -6,3 +6,9 @@
 
 # Add COMFAST CF-812AC (rtl8812bu) 
 #git clone https://github.com/0xACE8/rtl8812bu.git package/kernel/rtl8812bu
+
+sed -i '$a src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' feeds.conf.default
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '$a src-git ace https://github.com/0xACE8/ace-packages' feeds.conf.default
+sed -i '$a src-git sirpdboy https://github.com/sirpdboy/netspeedtest' feeds.conf.default
