56561bcf4e41136931c2b2f689822f88b29dcff9
0xACE8 <19428059+0xACE8@users.noreply.github.com>
2023-08-11 03:46:18 -0700

    Update 4rmsr.f33ds.sh

--- 4rmsr/4rmsr.f33ds.sh
+++ 4rmsr/4rmsr.f33ds.sh
@@ -1,5 +1,5 @@
-src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2
-src-git kenzo https://github.com/kenzok8/openwrt-packages
-src-git small https://github.com/kenzok8/small
-src-git ace https://github.com/0xACE8/ace-packages
-src-git sirpdboy https://github.com/sirpdboy/netspeedtest
+sed -i '$a src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' feeds.conf.default
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '$a src-git ace https://github.com/0xACE8/ace-packages' feeds.conf.default
+sed -i '$a src-git sirpdboy https://github.com/sirpdboy/netspeedtest' feeds.conf.default
