d1d729b669fd58bfa7f348086e0399c1c195045a
0xACE8 <19428059+0xACE8@users.noreply.github.com>
2023-08-11 03:12:34 -0700

    Create 4rmsr.f33ds.sh

---
+++ 4rmsr/4rmsr.f33ds.sh
@@ -0,0 +1,5 @@
+src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2
+src-git kenzo https://github.com/kenzok8/openwrt-packages
+src-git small https://github.com/kenzok8/small
+src-git ace https://github.com/0xACE8/ace-packages
+src-git sirpdboy https://github.com/sirpdboy/netspeedtest
