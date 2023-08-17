976ca0be35789dc01839255cf60cae332cd0ae48
0xACE8 <19428059+0xACE8@users.noreply.github.com>
2023-08-16 21:40:42 -0700

    Update x6ui_cu570m1z3.sh

--- x6u/x6ui_cu570m1z3.sh
+++ x6u/x6ui_cu570m1z3.sh
@@ -7,19 +7,24 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.177.78/g' package/base-files/files/bin/config_generate
 
-# Modify Hostname
+# Set etc/openwrt_release
 sed -i 's/ImmortalWrt/Openwrt_Bypass/g' package/base-files/files/bin/config_generate
+sed -i "s|DISTRIB_REVISION='%R'|DISTRIB_REVISION='R$(date +%Y.%m.%d)'|g" package/base-files/files/etc/openwrt_release
+echo "DISTRIB_SOURCECODE='openwrt'" >>package/base-files/files/etc/openwrt_release
+sed -i 's/ImmortalWrt/OpenWrt/g' include/version.mk
+sed -i 's/ImmortalWrt/OpenWrt/g' config/Config-images.in
+sed -i 's/ImmortalWrt/OpenWrt/g' package/base-files/image-config.in
+#sed -i "s/OpenWrt /0xACE7 build $(TZ=UTC-3 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
 
 # Password is ********
 sed -i "s/root::0:0:99999:7:::/root:$1$4xKZB45Q$w0CPT5M6vBWbYNmSWuxfU.:19007:0:99999:7:::/g" package/base-files/files/etc/shadow
 
-# Modify builder
-#sed -i "s/OpenWrt /0xACE7 build $(TZ=UTC-3 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
-
 # Change language=auto to zh_cn
 sed -i 's/lang="auto"/lang="zh_cn"/g' package/emortal/default-settings/files/99-default-settings
 
-# interface lan6
+# interface
+sed -i "s/globals.ula_prefix='auto'/packet_steering='1'/g" package/base-files/files/bin/config_generate
+sed -i "/ip6assign='60'/d" 
 sed -i "9 i\uci set network.lan6=interface\nuci set network.lan6.proto='dhcpv6'\nuci set network.lan6.force_link='1'\nuci set network.lan6.reqaddress='try'\nuci set network.lan6.reqprefix='auto'\nuci set network.lan6.device='@lan'\nuci commit network\n" package/emortal/default-settings/files/99-default-settings
 
 # Time Zone to LA
@@ -36,22 +41,7 @@ sed -i 's/ntp1.aliyun.com/time.apple.com/g' package/emortal/default-settings/fil
 sed -i 's/"Argon 主题设置"/"主题设置"/g' feeds/luci/applications/luci-app-argon-config/po/zh_Hans/argon-config.po
 sed -i 's/"#5e72e4"/"#6600cc"/g' feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/css/cascade.css
 sed -i 's/"#483d8b"/"#f9a602"/g' feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/css/cascade.css
-sed -i 's/"Turbo ACC 网络加速"/"网络加速"/g' feeds/luci/applications/luci-app-turboacc/po/zh-cn/turboacc.po
-
-# Change dns server
-#sed -i "2i # network config" package/lean/default-settings/files/zzz-default-settings
-#sed -i "3i uci set network.lan.dns='127.0.0.1'"  package/lean/default-settings/files/zzz-default-settings
-#sed -i "4i uci set network.lan.delegate='0'" package/lean/default-settings/files/zzz-default-settings
-#sed -i "5i uci set network.lan.gateway='192.168.177.1'" package/lean/default-settings/files/zzz-default-settings
-#sed -i "6i # ipv6 network config" package/lean/default-settings/files/zzz-default-settings
-#sed -i "7i uci set network.lan6='interface'" package/lean/default-settings/files/zzz-default-settings
-#sed -i "8i uci set network.lan6.proto='dhcpv6'" package/lean/default-settings/files/zzz-default-settings
-#sed -i "9i uci set network.lan6.ifname='eth0'" package/lean/default-settings/files/zzz-default-settings
-#sed -i "10i uci set network.lan6.delegate='0'" package/lean/default-settings/files/zzz-default-settings
-#sed -i "11i uci set network.lan6.reqaddress='try'" package/lean/default-settings/files/zzz-default-settings
-#sed -i "12i uci set network.lan6.reqprefix='auto'" package/lean/default-settings/files/zzz-default-settings
-#sed -i "13i uci set network.lan6.peerdns='0'" package/lean/default-settings/files/zzz-default-settings
-#sed -i "14i uci set network.lan6.dns='::1'" package/lean/default-settings/files/zzz-default-settings
+#sed -i 's/"Turbo ACC 网络加速"/"网络加速"/g' feeds/luci/applications/luci-app-turboacc/po/zh-cn/turboacc.po
 #sed -i '/revert_dns/d' feeds/luci/applications/luci-app-turboacc/root/etc/init.d/turboacc
 
 # Boost UDP
