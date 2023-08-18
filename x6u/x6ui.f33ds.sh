28f0ca0ed066ebc37a925598f47b434fe7188556
0xACE8 <19428059+0xACE8@users.noreply.github.com>
2023-08-18 05:39:41 -0700

    Update x6ui.f33ds.sh

--- x6u/x6ui.f33ds.sh
+++ x6u/x6ui.f33ds.sh
@@ -1,3 +1,2 @@
 sed -i '$a src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' feeds.conf.default
-sed -i '$a src-git ace https://github.com/0xACE8/ace8-packages' feeds.conf.default
 sed -i '$a src-git netspeedtest https://github.com/sirpdboy/netspeedtest' feeds.conf.default
