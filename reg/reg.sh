024e9b49b897dccc10a875a2af7b351f23e323b2
0xACE8 <19428059+0xACE8@users.noreply.github.com>
2023-08-22 03:22:51 -0700

    Update reg.sh

--- reg/reg.sh
+++ reg/reg.sh
@@ -1,3 +1,11 @@
-# Add a feed source
+src-git small https://github.com/kenzok8/small
+src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2
+src-git passwall https://github.com/xiaorouji/openwrt-passwall;luci
+#src-git ace https://github.com/0xACE8/ace-packages
+#src-git kenzo https://github.com/kenzok8/openwrt-packages
+#src-git small https://github.com/kenzok8/small
+src-git ntfs3 https://github.com/xymopen/openwrt-ntfs3
+src-git netspeedtest https://github.com/sirpdboy/netspeedtest
+
 
 
