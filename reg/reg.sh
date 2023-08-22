6b85eb445812d3a50c2794404f1d3b9fe9d5fb7e
0xACE8 <19428059+0xACE8@users.noreply.github.com>
2023-08-22 03:33:11 -0700

    Update reg.sh

--- reg/reg.sh
+++ reg/reg.sh
@@ -1,11 +1,10 @@
-src-git small https://github.com/kenzok8/small
-src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2
-src-git passwall https://github.com/xiaorouji/openwrt-passwall;luci
-#src-git ace https://github.com/0xACE8/ace-packages
-#src-git kenzo https://github.com/kenzok8/openwrt-packages
-#src-git small https://github.com/kenzok8/small
-src-git ntfs3 https://github.com/xymopen/openwrt-ntfs3
-src-git netspeedtest https://github.com/sirpdboy/netspeedtest
-
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '$a src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' feeds.conf.default
+sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall;luci' feeds.conf.default
+sed -i '$a src-git ntfs3 https://github.com/xymopen/openwrt-ntfs3' feeds.conf.default
+sed -i '$a src-git netspeedtest https://github.com/sirpdboy/netspeedtest' feeds.conf.default
+#sed -i '$a src-git ace https://github.com/0xACE8/ace-packages' feeds.conf.default
+#sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+#sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
 
