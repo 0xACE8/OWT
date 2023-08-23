0dcf8e45a16858ec53994073e8d635f180e1576e
0xACE8 <19428059+0xACE8@users.noreply.github.com>
2023-08-23 16:03:28 -0700

    Update reg_customize.sh

--- reg/reg_customize.sh
+++ reg/reg_customize.sh
@@ -62,7 +62,7 @@ git clone https://github.com/0xACE8/luci-app-syncdial feeds/luci/applications/lu
 git clone https://github.com/kenzok78/luci-theme-argone.git package/luci-theme-argone
 git clone https://github.com/kenzok78/luci-app-argone-config.git package/luci-app-argone-config
 
-sed -i 's/"Argonne 主题设置"/"主题设置"/g' package/luci-app-argone-config/po/zh-cn/argone-config.po
+sed -i 's/"Argone 主题设置"/"主题设置"/g' package/luci-app-argone-config/po/zh-cn/argone-config.po
 rm -rf package/luci-app-argone-config/po/zh-Hans
 sed -i 's/5e72e4/ff6900/g' package/luci-app-argone-config/root/etc/config/argone
 sed -i 's/483d8b/ff6900/g' package/luci-app-argone-config/root/etc/config/argone
