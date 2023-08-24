12a454d0031d17ffe15ffd37dd32087dd62b9b08
0xACE8 <19428059+0xACE8@users.noreply.github.com>
2023-08-23 21:43:59 -0700

    Update reg.sh

--- reg/reg.sh
+++ reg/reg.sh
@@ -3,10 +3,5 @@ sed -i '$a src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' fee
 sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall;luci' feeds.conf.default
 sed -i '$a src-git ntfs3 https://github.com/xymopen/openwrt-ntfs3' feeds.conf.default
 sed -i '$a src-git netspeedtest https://github.com/sirpdboy/netspeedtest' feeds.conf.default
-sed -i '$a src-git lucky https://github.com/sirpdboy/luci-app-lucky' feeds.conf.default
-sed -i '$a src-git opentopd  https://github.com/sirpdboy/sirpdboy-package' feeds.conf.default
 #sed -i '$a src-git ace https://github.com/0xACE8/ace-packages' feeds.conf.default
 #sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-#sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
-
-
