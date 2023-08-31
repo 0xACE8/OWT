2c68568c100c308c7c8e94ddc8f9d07d69520db3
0xACE8 <19428059+0xACE8@users.noreply.github.com>
2023-08-31 03:42:57 -0700

    Update x6ui_cu570m1z3.sh

--- x6u/x6ui_cu570m1z3.sh
+++ x6u/x6ui_cu570m1z3.sh
@@ -48,8 +48,6 @@ sed -i 's/ntp.tencent.com/ntp.ntsc.ac.cn/g' package/emortal/default-settings/fil
 sed -i 's/"Argon 主题设置"/"主题设置"/g' feeds/luci/applications/luci-app-argon-config/po/zh_Hans/argon-config.po
 sed -i 's/"#5e72e4"/"#6600cc"/g' feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/css/cascade.css
 sed -i 's/"#483d8b"/"#f9a602"/g' feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/css/cascade.css
-#sed -i 's/"Turbo ACC 网络加速"/"网络加速"/g' feeds/luci/applications/luci-app-turboacc/po/zh-cn/turboacc.po
-#sed -i '/revert_dns/d' feeds/luci/applications/luci-app-turboacc/root/etc/init.d/turboacc
 
 # Boost UDP
 echo '# optimize udp' >>package/base-files/files/etc/sysctl.d/10-default.conf
@@ -71,6 +69,30 @@ rm -rf feeds/luci/applications/luci-app-udp2raw
 git clone https://github.com/0xACE8/luci-app-udp2raw feeds/luci/applications/luci-app-udp2raw
 rm -rf feeds/ace8/luci-app-udpspeeder
 git clone https://github.com/0xACE8/luci-app-udpspeeder feeds/ace8/luci-app-udpspeeder
+
+# turboacc
+#mkdir -p turboacc_tmp ./package/turboacc
+#cd turboacc_tmp 
+#git clone https://github.com/chenmozhijin/turboacc -b package
+#cd ../package/turboacc
+#git clone https://github.com/fullcone-nat-nftables/nft-fullcone
+#git clone https://github.com/chenmozhijin/turboacc
+#mv ./turboacc/luci-app-turboacc ./luci-app-turboacc
+#rm -rf ./turboacc
+#sed -i 's/Turbo ACC 网络加速/网络加速/g' luci-app-turboacc/po/zh-cn/turboacc.po
+#cd ../..
+#cp -f turboacc_tmp/turboacc/hack-5.15/952-add-net-conntrack-events-support-multiple-registrant.patch ./target/linux/generic/hack-5.15/952-add-net-conntrack-events-support-multiple-registrant.patch
+#cp -f turboacc_tmp/turboacc/hack-5.15/953-net-patch-linux-kernel-to-support-shortcut-fe.patch ./target/linux/generic/hack-5.15/953-net-patch-linux-kernel-to-support-shortcut-fe.patch
+#cp -f turboacc_tmp/turboacc/pending-5.15/613-netfilter_optional_tcp_window_check.patch ./target/linux/generic/pending-5.15/613-netfilter_optional_tcp_window_check.patch
+#rm -rf ./package/libs/libnftnl ./package/network/config/firewall4 ./package/network/utils/nftables
+#mkdir -p ./package/network/config/firewall4 ./package/libs/libnftnl ./package/network/utils/nftables
+#cp -r ./turboacc_tmp/turboacc/shortcut-fe ./package/turboacc
+#cp -RT ./turboacc_tmp/turboacc/firewall4-$(grep -o 'FIREWALL4_VERSION=.*' ./turboacc_tmp/turboacc/version | cut -d '=' -f 2)/firewall4 ./package/network/config/firewall4
+#cp -RT ./turboacc_tmp/turboacc/libnftnl-$(grep -o 'LIBNFTNL_VERSION=.*' ./turboacc_tmp/turboacc/version | cut -d '=' -f 2)/libnftnl ./package/libs/libnftnl
+#cp -RT ./turboacc_tmp/turboacc/nftables-$(grep -o 'NFTABLES_VERSION=.*' ./turboacc_tmp/turboacc/version | cut -d '=' -f 2)/nftables ./package/network/utils/nftables
+#rm -rf turboacc_tmp
+#echo "# CONFIG_NF_CONNTRACK_CHAIN_EVENTS is not set" >> target/linux/generic/config-5.15
+#echo "# CONFIG_SHORTCUT_FE is not set" >> target/linux/generic/config-5.15
 
 # Update dnscrypt-proxy2
 #rm -rf feeds/packages/net/dnscrypt-proxy2
