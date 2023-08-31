80363dafc24165472eefbaf0e985c0580476f691
0xACE8 <19428059+0xACE8@users.noreply.github.com>
2023-08-31 06:08:02 -0700

    Update x6ui_cu570m1z3.sh

--- x6u/x6ui_cu570m1z3.sh
+++ x6u/x6ui_cu570m1z3.sh
@@ -11,7 +11,7 @@ sed -i 's/192.168.1.1/192.168.177.80/g' package/base-files/files/bin/config_gene
 sed -i 's/ImmortalWrt/Bypass_Router/g' package/base-files/files/bin/config_generate
 sed -i "s|DISTRIB_REVISION='%R'|DISTRIB_REVISION='R$(date +%Y.%m.%d)'|g" package/base-files/files/etc/openwrt_release
 echo "DISTRIB_SOURCECODE='openwrt'" >>package/base-files/files/etc/openwrt_release
-sed -i 's/ImmortalWrt/OpenWrt/g' include/version.mk
+#sed -i 's/ImmortalWrt/OpenWrt/g' include/version.mk
 #sed -i 's/ImmortalWrt/OpenWrt/g' config/Config-images.in
 #sed -i 's/ImmortalWrt/OpenWrt/g' package/base-files/image-config.in
 #sed -i "s/OpenWrt /0xACE7 build $(TZ=UTC-3 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
@@ -31,8 +31,20 @@ sed -i "9 i\uci set network.lan6=interface\nuci set network.lan6.proto='dhcpv6'\
 # firewall
 sed -i "17 i\ \tlist network\t\t'lan6'" package/network/config/firewall/files/firewall.config
 sed -i "s/ \t/\t/g" package/network/config/firewall/files/firewall.config
-# dhcp
-#sed -i "s/ \t/\t/g" package/network/services/dnsmasq/files/dhcp.conf
+# DHCP
+sed -i 's/boguspriv\t1/boguspriv\t0/g' package/network/services/dnsmasq/files/dhcp.conf
+sed -i 's/nonegcache\t0/nonegcache\t1/g' package/network/services/dnsmasq/files/dhcp.conf
+sed -i 's/authoritative\t1/authoritative\t0/g' package/network/services/dnsmasq/files/dhcp.conf
+sed -i 's/readethers\t1/readethers\t0/g' package/network/services/dnsmasq/files/dhcp.conf
+sed -i 's/filter_aaaa\t0/filter_aaaa\t1/g' package/network/services/dnsmasq/files/dhcp.conf
+sed -i 's/cachesize\t8000/cachesize\t10000/g' package/network/services/dnsmasq/files/dhcp.conf
+sed -i 's/cachesize\t8000/cachesize\t10000/g' package/network/services/dnsmasq/files/dhcp.conf
+sed -i "27 i\ \toption localuse\t1\n\tlist server\t'127.0.0.1#5335'\n\toption noresolv\t1\n\toption nohosts\t1\n\toption serversfile\t'/etc/dnsmasq.d/accelerated-domains.china.conf'\n\toption allservers\t1" package/network/services/dnsmasq/files/dhcp.conf
+sed -i "45 i\ \toption dhcpv4\t'server'\n\toption ra_management\t'1'\n\toption ignore\t'1'\n\toption dynamicdhcp\t'0'" package/network/services/dnsmasq/files/dhcp.conf
+sed -i 's/option ignore\t1/option ignore\t1\n/g' package/network/services/dnsmasq/files/dhcp.conf
+sed -i "53 i\ \nconfig odhcpd\t'odhcpd'\n\toption maindhcp\t'0'\n" package/network/services/dnsmasq/files/dhcp.conf
+sed -i "s/ \t/\t/g" package/network/services/dnsmasq/files/dhcp.conf
+echo "interface updated."
 
 # Time Zone to LA
 sed -i 's/CST-8/CST-4/g' package/emortal/default-settings/files/99-default-settings-chinese
@@ -46,8 +58,11 @@ sed -i 's/ntp.tencent.com/ntp.ntsc.ac.cn/g' package/emortal/default-settings/fil
 
 # Change luci list name
 sed -i 's/"Argon 主题设置"/"主题设置"/g' feeds/luci/applications/luci-app-argon-config/po/zh_Hans/argon-config.po
-sed -i 's/"#5e72e4"/"#6600cc"/g' feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/css/cascade.css
-sed -i 's/"#483d8b"/"#f9a602"/g' feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/css/cascade.css
+#sed -i 's/"#5e72e4"/"#6600cc"/g' feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/css/cascade.css
+#sed -i 's/"#483d8b"/"#f9a602"/g' feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/css/cascade.css
+sed -i 's/5e72e4/6600cc/g' package/luci-app-argone-config/root/etc/config/argone
+sed -i 's/483d8b/f9a602/g' package/luci-app-argone-config/root/etc/config/argone
+sed -i 's/normal/light/g' package/luci-app-argone-config/root/etc/config/argone
 
 # Boost UDP
 echo '# optimize udp' >>package/base-files/files/etc/sysctl.d/10-default.conf
@@ -56,6 +71,9 @@ echo 'net.core.rmem_default=26214400' >>package/base-files/files/etc/sysctl.d/10
 echo 'net.core.wmem_max=26214400' >>package/base-files/files/etc/sysctl.d/10-default.conf
 echo 'net.core.wmem_default=26214400' >>package/base-files/files/etc/sysctl.d/10-default.conf
 echo 'net.core.netdev_max_backlog=2048' >>package/base-files/files/etc/sysctl.d/10-default.conf
+
+# change some tips
+sed -i 's/"终端"/"TTYD 终端"/g' feeds/luci/applications/luci/applications/luci-app-ttyd/po/zh_Hans/ttyd.po
 
 # update gamespeeder
 git clone https://github.com/sirpdboy/luci-app-advanced package/luci-app-advanced
@@ -98,13 +116,14 @@ git clone https://github.com/0xACE8/luci-app-udpspeeder feeds/ace8/luci-app-udps
 #rm -rf feeds/packages/net/dnscrypt-proxy2
 #git clone https://github.com/0xACE8/dnscrypt-proxy2.git feeds/packages/net/dnscrypt-proxy2
 sed -i 's/127.0.0.53:53/127.0.0.1:5335/g' feeds/packages/net/dnscrypt-proxy2/Makefile
-#sed -i '59 i\ \tsed -i "s\/ipv6_servers = false\/ipv6_servers = true\/g" \$(1)\/etc\/dnscrypt-proxy2\/dnscrypt-proxy.toml' feeds/packages/net/dnscrypt-proxy2/Makefile
-#sed -i '60 i\ \tsed -i "s\/odoh_servers = false\/odoh_servers = true\/g" \$(1)\/etc\/dnscrypt-proxy2\/dnscrypt-proxy.toml' feeds/packages/net/dnscrypt-proxy2/Makefile
-#sed -i '61 i\ \tsed -i "s\/http3 = false\/http3 = true\/g" \$(1)\/etc\/dnscrypt-proxy2\/dnscrypt-proxy.toml' feeds/packages/net/dnscrypt-proxy2/Makefile
-#sed -i '62 i\ \tsed -i "s\/cache_size = 4096\/cache_size = 10240\/g" \$(1)\/etc\/dnscrypt-proxy2\/dnscrypt-proxy.toml' feeds/packages/net/dnscrypt-proxy2/Makefile
+sed -i '59 i\ \tsed -i "s\/ipv6_servers = false\/ipv6_servers = true\/g" \$(1)\/etc\/dnscrypt-proxy2\/dnscrypt-proxy.toml' feeds/packages/net/dnscrypt-proxy2/Makefile
+sed -i '60 i\ \tsed -i "s\/odoh_servers = false\/odoh_servers = true\/g" \$(1)\/etc\/dnscrypt-proxy2\/dnscrypt-proxy.toml' feeds/packages/net/dnscrypt-proxy2/Makefile
+sed -i '61 i\ \tsed -i "s\/http3 = false\/http3 = true\/g" \$(1)\/etc\/dnscrypt-proxy2\/dnscrypt-proxy.toml' feeds/packages/net/dnscrypt-proxy2/Makefile
+sed -i '62 i\ \tsed -i "s\/cache_size = 4096\/cache_size = 10240\/g" \$(1)\/etc\/dnscrypt-proxy2\/dnscrypt-proxy.toml' feeds/packages/net/dnscrypt-proxy2/Makefile
 #sed -i '63 i\ \tsed -i "s/# proxy/proxy/g" $(1)/etc/dnscrypt-proxy2/dnscrypt-proxy.toml' feeds/packages/net/dnscrypt-proxy2/Makefile
 #sed -i '64 i\ \tsed -i "s/127.0.0.1:9050/127.0.0.1:1070/g" $(1)/etc/dnscrypt-proxy2/dnscrypt-proxy.toml' feeds/packages/net/dnscrypt-proxy2/Makefile
 #sed -i '65 i\ \tsed -i "s/# blocked_names_file/blocked_names_file/g" $(1)/etc/dnscrypt-proxy2/dnscrypt-proxy.toml' feeds/packages/net/dnscrypt-proxy2/Makefile
+echo "dnscrypt-proxy2 updated."
 
 # Change to my banner
 sudo rm package/base-files/files/etc/banner
