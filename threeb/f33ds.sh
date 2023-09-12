240ea506690c5469d3c46ddd16ac2f7c7174e6e9
0xACE8 <19428059+0xACE8@users.noreply.github.com>
2023-09-12 04:53:42 -0700

    Update f33ds.sh

--- threeb/f33ds.sh
+++ threeb/f33ds.sh
@@ -1,8 +1,8 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-#echo 'src-git ace https://github.com/0xACE8/ace-packages' >>feeds.conf.default
-#echo 'src-git roacn https://github.com/roacn/openwrt-packages' >>feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+echo 'src-git ace https://github.com/0xACE8/ace-packages' >>feeds.conf.default
+echo 'src-git netspeedtest https://github.com/sirpdboy/netspeedtest' >>feeds.conf.default
 
 # Add COMFAST CF-812AC (rtl8812bu) 
 #git clone https://github.com/0xACE8/rtl8812bu.git package/kernel/rtl8812bu
