163e2d7154137b68608ef126d336fe13dfd7881a
0xACE8 <19428059+0xACE8@users.noreply.github.com>
2023-09-14 07:58:17 -0700

    Update ttc_customize.sh

--- ttc/ttc_customize.sh
+++ ttc/ttc_customize.sh
@@ -5,14 +5,14 @@
 # Author: 0xACE7
 #=================================================
 #1. Modify default IP
-sed -i 's/192.168.1.1/192.168.50.60/g' package/base-files/files/bin/config_generate
-sed -i "s/ip6assign='60'/ip6assign='64'/g" package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.50.68/g' package/base-files/files/bin/config_generate
+#sed -i "s/ip6assign='60'/ip6assign='64'/g" package/base-files/files/bin/config_generate
 
 #2. Modify Hostname
 sed -i 's/ImmortalWrt/TimeCloud/g' package/base-files/files/bin/config_generate
 
 #3. Modify Password to ********
-sed -i 's/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/root:$1$4xKZB45Q$w0CPT5M6vBWbYNmSWuxfU.:19007:0:99999:7:::/g' package/emortal/default-settings/files/99-default-settings
+sed -i 's/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/root::19007:0:99999:7:::/g' package/emortal/default-settings/files/99-default-settings
 
 #4. Modify builder
 #sed -i 's/immortalwrt_luci /0xACE7 build $(TZ=UTC-3 date "+%Y.%m.%d") @ OpenWrt /g' /etc/opkg/distfeeds.conf
@@ -42,14 +42,14 @@ sed -i 's/ntp.tencent.com/ntp.ntsc.ac.cn/g' package/emortal/default-settings/fil
 
 #8. Change luci list name and xiaomi logo color
 #sed -i 's/"Design 主题设置"/"主题设置"/g' feeds/ace/luci-app-design-config/po/zh-cn/design-config.po
-sed -i 's/"Argonne 主题设置"/"主题设置"/g' feeds/ace/luci-app-argone-config/po/zh-cn/argone-config.po
+sed -i 's/"Argone 主题设置"/"主题设置"/g' feeds/ace/luci-app-argone-config/po/zh-cn/argone-config.po
 #sed -i 's/"Turbo ACC 网络加速"/"网络加速"/g' feeds/luci/applications/luci-app-turboacc/po/zh-cn/turboacc.po
 #sed -i 's/"#5e72e4"/"#ff6900"/g' feeds/ace/luci-theme-argone/htdocs/luci-static/argone/css/cascade.css
 #sed -i 's/"#483d8b"/"#ff6900"/g' feeds/ace/luci-theme-argone/htdocs/luci-static/argone/css/cascade.css
 
 #9. Change to my banner and argone logo to mi logo
-sudo rm package/emortal/default-settings/files/openwrt_banner
-wget https://raw.githubusercontent.com/0xACE8/OWT/main/reg/banner -O package/emortal/default-settings/files/openwrt_banner
+#sudo rm package/emortal/default-settings/files/openwrt_banner
+#wget https://raw.githubusercontent.com/0xACE8/OWT/main/reg/banner -O package/emortal/default-settings/files/openwrt_banner
 
 #rm -rf feeds/ace/luci-theme-argone/htdocs/luci-static/argone/img/argone.svg
 #wget --no-check-certificate -O feeds/ace/luci-theme-argone/htdocs/luci-static/argone/img/argone.svg "https://upload.wikimedia.org/wikipedia/commons/a/ae/Xiaomi_logo_%282021-%29.svg"
