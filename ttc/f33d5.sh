a6a36b971d5ca81d9593d5599c5fabded03913ad
0xACE8 <19428059+0xACE8@users.noreply.github.com>
2023-09-18 08:16:06 -0700

    Update f33d5.sh

--- ttc/f33d5.sh
+++ ttc/f33d5.sh
@@ -1,4 +1,8 @@
 sed -i '$a src-git ace https://github.com/0xACE8/4c3-p4ck463' feeds.conf.default
 sed -i '$a src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' feeds.conf.default
+sed -i '$a src-git packages https://github.com/padavanonly/packages.git;openwrt-18.06' feeds.conf.default
+sed -i '$a src-git luci https://github.com/padavanonly/luci.git;openwrt-18.06-k5.4' feeds.conf.default
+sed -i '$a src-git routing https://github.com/openwrt/routing.git' feeds.conf.default
+sed -i '$a src-git telephony https://github.com/openwrt/telephony.git' feeds.conf.default
 sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' feeds.conf.default
 sed -i '$a src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main' feeds.conf.default
